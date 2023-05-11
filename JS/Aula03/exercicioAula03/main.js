860929c3d060786ccb1f766ce8d0e7fb1dc641ad
limagio <giovanamflima@gmail.com>
2023-05-10 21:11:46 -0300

    ATT

---
+++ JS/Aula03/exercicioAula03/main.js
@@ -0,0 +1,20 @@
+var fruta1= "                       pitaya                             ";
+var fruta2= "Morango";
+var fruta3= "Tomate";
+var fruta4= "Melao";
+var fruta5= "Melancia";
+const novaMensagem = fruta4.replace('Melao', 'Beterraba') 
+// Acessando os 3 primeiros caracteres
+console.log(fruta1.trim());
+console.log(fruta2.length);
+console.log(fruta3.substring(0,3))
+console.log(novaMensagem.toLowerCase());
+console.log()
+//
+
+
+
+
+
+
+
