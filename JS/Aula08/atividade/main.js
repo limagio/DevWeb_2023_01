209f9020f7d1e3451b8becc06ea84dbe11c77e64
limagio <giovanamflima@gmail.com>
2023-05-18 21:39:44 -0300

    envio

--- JS/Aula08/atividade/main.js
+++ JS/Aula08/atividade/main.js
@@ -1,37 +1,36 @@
-let aluno = {
-    primeiroNome: "Abraao",
-    nota: 8,
-    idade: 102,
-    altura: 3.15,
-    hobbies1: ["ping pong","Castor de Andrade", "Futebol"],
-    personalidade: "jogador caro",
-    endereco: {
-        numeroCasa: 34,
-        nomeRua: "X",
-    }
-}
-
-console.log(aluno)
-console.log(aluno.hobbies1)
-console.log(aluno.hobbies1[1])
-console.log(aluno.endereco.numeroCasa)
-
-console.clear()
-const{ 
-    primeiroNome,
-    idade,
-    nota
-} = aluno;
-
-console.log(aluno.nota)
-console.log(nota)
-
-
-console.clear
-    const alunosIos = [
-        {
-            nomeAluno: "Thainá",
-            idade: 35
-            nota:10
-        }
-    ]
+   // Funções
+function addNums(num1 = 1, num2 = 1) {
+    return num1 + num2;
+   }
+   let x = addNums(4, 5); // Chamada da função addNums
+   console.log(x);
+   let z = myFunc(4, 5); // Chamada da função myFunc
+   console.log(z);
+   function myFunc(num1, num2) {
+    return num1 * num2;
+   }
+   //Arrow functions
+console.clear();
+const hello = () => {
+return 'Olá Arrow Function!';
+};
+console.log(hello); // Retorna o objeto função
+console.log(hello()); // Executa a função e imprime a string no return
+console.clear();
+const addNums2 = (num1 = 1, num2 = 1) => {
+return num1 + num2;
+};
+let soma = addNums2(5, 10);
+console.log(soma);
+let marca = 'Fiat';
+const carro = {
+type: 'Fiat',
+model: '500',
+color: 'white',
+};
+const pessoa = {
+firstName: 'John',
+lastName: 'Doe',
+age: 50,
+eyeColor: 'blue',
+};
---
+++ JS/Aula08/exercicio/main.js
@@ -0,0 +1,12 @@
+const boasVindas = () => {
+    alert('Bem vinde i tudo bein');
+    console.log('Bem vindo a nossa página');
+};
+
+var a = 1;
+var b = 2;
+var c = 3;
+
+const delta = (b * b) - (4 * a * c );
+
+
