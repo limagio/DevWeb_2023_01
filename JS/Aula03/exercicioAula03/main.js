c3d65f2a2f98328290676cb5233225c7aa5ffdc9
limagio <giovanamflima@gmail.com>
2023-06-18 17:19:03 -0300

    aula03

--- JS/Aula03/exercicioAula03/main.js
+++ JS/Aula03/exercicioAula03/main.js
@@ -4,12 +4,16 @@ var fruta3= "Tomate";
 var fruta4= "Melao";
 var fruta5= "Melancia";
 const novaMensagem = fruta4.replace('Melao', 'Beterraba') 
-// Acessando os 3 primeiros caracteres
+const templateString = `As frutas são ${fruta1}`
+const templateString1 = `${fruta2}`
+const templateString2 = `${fruta3}`
+const templateString3 = `${fruta4}`
+const templateString4 = `${fruta5}`
 console.log(fruta1.trim());
 console.log(fruta2.length);
 console.log(fruta3.substring(0,3))
 console.log(novaMensagem.toLowerCase());
-console.log()
+console.log(templateString + " " + templateString1 + " " + templateString2 + " " + templateString3 + templateString4)
 //
 
 
