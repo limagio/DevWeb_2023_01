6abc581fc1d1f728dc1d6d82fc12a65c6a5d80fe
limagio <giovanamflima@gmail.com>
2023-06-18 17:38:02 -0300

    Aula04

--- JS/Aula04/atividadeAula04/main.js
+++ JS/Aula04/atividadeAula04/main.js
@@ -1,7 +1,11 @@
-if(8>7){
-    console.log("Aprovado")
-    }else if(8< 6.9 && 8>5){
-console.log("Aprovado por conselho")
-    }else{
-        console.log("Reprovado")
+var numero1 = 5
+var numero2 = 7
+function verificarNumero() {
+    if (numero1 > numero2) {
+        console.log(numero1 + " é maior.");
+    } else {
+        console.log(numero2 + " é maior.");
     }
+}
+
+verificarNumero()
