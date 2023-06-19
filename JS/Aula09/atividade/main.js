c0fb5d023a3fd0f9bbd2a7387c5a8b512c07597f
limagio <giovanamflima@gmail.com>
2023-06-18 22:15:17 -0300

    licao9

--- JS/Aula09/atividade/main.js
+++ JS/Aula09/atividade/main.js
@@ -1,3 +1,21 @@
-const number = [17, 43, 8, 4, 97, 56, 29];
-console.log(number);
+const newLocal = [17, 43, 8, 4, 97, 56, 29];
+const verificarParImpar = (numero) => {
+  if (numero % 2 === 0) {
+    console.log(`${numero} é par.`);
+  } else {
+    console.log(`${numero} é ímpar.`);
+  }
+};
+console.log("")
+console.log("")
 
+const array = [17, 43, 8, 4, 97, 56, 29, 3, 75, 34, 88, 82, 61];
+
+const valoresEntre20e80 = array.filter((numero) => {
+  if (numero >= 20 && numero <= 80) {
+    return true;
+  } else {
+    return false;
+  }
+});
+console.log("");
