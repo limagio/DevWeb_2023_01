8ef4e7b8a1f8385739ccf339184fb4e5454f590b
limagio <giovanamflima@gmail.com>
2023-06-21 21:51:54 -0300

    react

---
+++ REACT/React_Sala/03_aula/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Exemplo from './Components/Exemplo';
+import Header from './Components/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Exemplo />
+        </div>
+    );
+}
+
+export default App;
+
+
