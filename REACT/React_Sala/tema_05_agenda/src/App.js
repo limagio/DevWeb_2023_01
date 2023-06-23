6176e80af84585d5b1c4970ccc9139bbcc8da0bd
limagio <giovanamflima@gmail.com>
2023-06-23 20:00:08 -0300

    TCCteste

---
+++ REACT/React_Sala/tema_05_agenda/src/App.js
@@ -0,0 +1,17 @@
+return (
+    <div className="container">
+        <h1>Olá, Pessoal!</h1>
+        <p>Agenda de compromissos.</p>
+    </div>
+);
+import Header from './Components/Header';
+
+function App() {
+    return (
+        <div className="container">
+            <Header title="tarefas"/>
+        </div>
+    );
+}
+
+export default App;
